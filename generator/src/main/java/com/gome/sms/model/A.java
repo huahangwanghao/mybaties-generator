f9affe1a4b7c81aefa03c1f9b45adfbaf8074897
wanghao2 <wanghao2@gomeholdings.com>
2017-01-04 11:04:25 +0800

    add　get/set

--- generator/src/main/java/com/gome/sms/model/A.java
+++ generator/src/main/java/com/gome/sms/model/A.java
@@ -5,4 +5,21 @@ package com.gome.sms.model;
  */
 public class A {
     private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
